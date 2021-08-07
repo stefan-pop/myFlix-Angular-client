db4874ddd9951446427efeff9633520ad1b7dfc9
Stefan Pop <stefan9161@gmail.com>
2021-08-07 18:43:28 +0200

    initial commit

---
+++ src/app/fetch-api-data.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/internal/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export class FetchApiDataService {
+  // ...
+}
