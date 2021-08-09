3e2fbd7eff2d9a82271ba0f35765dcf3863e42af
Stefan Pop <stefan9161@gmail.com>
2021-08-09 14:09:25 +0200

    created the component responsible for user's registration using features of Angular Material

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+// This import brings in the API calls from the service file
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+// This import to closes the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+
+// This import is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
-  selector: 'app-user-registration-form',
-  templateUrl: './user-registration-form.component.html',
-  styleUrls: ['./user-registration-form.component.scss']
+	selector: 'app-user-registration-form',
+	templateUrl: './user-registration-form.component.html',
+	styleUrls: ['./user-registration-form.component.scss']
 })
 export class UserRegistrationFormComponent implements OnInit {
 
-  constructor() { }
+	@Input() userData = {username: '', pwd: '', email: '', birth_date: ''}
 
-  ngOnInit(): void {
-  }
+	constructor(
+		public fetchApiData: FetchApiDataService,
+		public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+		public snackBar: MatSnackBar) { }
 
+	ngOnInit(): void {
+	}
+
+	// Send the registration form inputs to the backend
+	registerUser(): void {
+		this.fetchApiData.userRegistration(this.userData).subscribe((response) => {
+			// Logic for successful registration (implement later)
+			this.dialogRef.close();
+			console.log(response);
+			this.snackBar.open(response, 'OK', {
+				duration: 2000
+			});
+		}, (response) => {
+			console.log(response);
+			this.snackBar.open(response, 'OK', {
+				duration: 2000
+			});
+		});
+	}
 }
