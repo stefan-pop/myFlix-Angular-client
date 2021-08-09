aea34476322441cb37df3b7a217e6b91d9f73f28
Stefan Pop <stefan9161@gmail.com>
2021-08-09 17:06:01 +0200

    created the user's login component similar to the registration component

---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, Input } from '@angular/core';
+// This import brings in the API calls from the service file
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+// This import to closes the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+
+// This import is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+	selector: 'app-user-login-form',
+	templateUrl: './user-login-form.component.html',
+	styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent implements OnInit {
+
+	@Input() userData = { username: '', pwd: '' }
+
+	constructor(
+		public fetchApiData: FetchApiDataService,
+		public dialogRef: MatDialogRef<UserLoginFormComponent>,
+		public snackBar: MatSnackBar) { }
+
+	ngOnInit(): void {
+	}
+
+	// Send the login form inputs to the backend
+	loginUser(): void {
+		this.fetchApiData.userLogin(this.userData).subscribe((response) => {
+			// Logic for successful registration (implement later)
+			this.dialogRef.close();
+			// Save token and user in local storage
+			localStorage.setItem('token', response.token);
+			localStorage.setItem('user', JSON.stringify(response.user));
+			console.log(response);
+			this.snackBar.open(response, 'OK', {
+				duration: 2000
+			});
+		}, (response) => {
+			console.log(response);
+			this.snackBar.open(response, 'OK', {
+				duration: 2000
+			});
+		});
+	}
+}
