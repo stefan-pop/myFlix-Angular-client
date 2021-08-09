674479b8e87a2ee7a76b5b0ca2f0ded9b49411e7
Stefan Pop <stefan9161@gmail.com>
2021-08-09 17:08:39 +0200

    created a method in the app component that opens the login component in an Angular Material dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -15,6 +16,14 @@ export class AppComponent {
 	// Open the dialog when the "Sign Up" button is clicked
 	openUserRegistrationDialog(): void {
 		this.dialog.open(UserRegistrationFormComponent, {
+			// Assign the dialog a width
+			width: '280px'
+		});
+	}
+
+	// Open the dialog when the "Login" button is clicked
+	openUserLoginDialog(): void {
+		this.dialog.open(UserLoginFormComponent, {
 			// Assign the dialog a width
 			width: '280px'
 		});
