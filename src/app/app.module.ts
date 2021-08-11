e39286baf4780132685ddec353b16e7c42d184d1
Stefan Pop <stefan9161@gmail.com>
2021-08-11 17:35:17 +0200

    imported MatIconModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { UserLoginFormComponent } from './user-login-form/user-login-form.compon
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { RouterModule, Routes } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 const appRoutes: Routes = [
 	{path: 'welcome', component: WelcomePageComponent},
@@ -46,7 +47,8 @@ const appRoutes: Routes = [
 		MatFormFieldModule,
 		MatDialogModule,
 		MatSnackBarModule,
-		FormsModule
+		FormsModule,
+		MatIconModule
   	],
   	providers: [],
   	bootstrap: [AppComponent]
