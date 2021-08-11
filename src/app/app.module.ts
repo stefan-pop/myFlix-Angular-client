44e732e66b7e368d3b224689801c374887568f39
Stefan Pop <stefan9161@gmail.com>
2021-08-11 17:39:40 +0200

    created a component for the profile of the user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { RouterModule, Routes } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const appRoutes: Routes = [
 	{path: 'welcome', component: WelcomePageComponent},
@@ -33,7 +34,8 @@ const appRoutes: Routes = [
 		UserRegistrationFormComponent,
 		UserLoginFormComponent,
   		MovieCardComponent,
-    	WelcomePageComponent
+    	WelcomePageComponent,
+     UserProfileComponent
   	],
   	imports: [
     	BrowserModule,
