6581739c598f5a249f261817d3d03b7a0fa32d85
Stefan Pop <stefan9161@gmail.com>
2021-08-11 15:55:38 +0200

    imported RouterModule and Routes and defined some paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,13 @@ import { UserRegistrationFormComponent } from './user-registration-form/user-reg
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+	{path: 'welcome', component: WelcomePageComponent},
+	{path: 'movies', component: MovieCardComponent},
+	{path: '', redirectTo: 'welcome', pathMatch: 'prefix'},
+];
 
 @NgModule({
   	declarations: [
@@ -30,6 +37,7 @@ import { WelcomePageComponent } from './welcome-page/welcome-page.component';
   	imports: [
     	BrowserModule,
     	AppRoutingModule,
+		RouterModule.forRoot(appRoutes),
 		HttpClientModule,
 		BrowserAnimationsModule,
 		MatInputModule,
