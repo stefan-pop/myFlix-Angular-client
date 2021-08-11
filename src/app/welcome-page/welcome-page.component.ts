2d63e534db21374e06c782ca8fc4bc28594d50ee
Stefan Pop <stefan9161@gmail.com>
2021-08-11 15:34:56 +0200

    moved the code from enrty file to WelcomePageComponent, so the entry file can manage the routing

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
+import { MovieCardComponent } from '../movie-card/movie-card.component';
 
 @Component({
-  selector: 'app-welcome-page',
-  templateUrl: './welcome-page.component.html',
-  styleUrls: ['./welcome-page.component.scss']
+	selector: 'app-welcome-page',
+	templateUrl: './welcome-page.component.html',
+	styleUrls: ['./welcome-page.component.scss']
 })
 export class WelcomePageComponent implements OnInit {
 
-  constructor() { }
+	constructor(public dialog: MatDialog) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
+		// Open the dialog when the "Sign Up" button is clicked
+	openUserRegistrationDialog(): void {
+		this.dialog.open(UserRegistrationFormComponent, {
+			// Assign the dialog a width
+			width: '280px'
+		});
+	}
+
+	// Open the dialog when the "Login" button is clicked
+	openUserLoginDialog(): void {
+		this.dialog.open(UserLoginFormComponent, {
+			// Assign the dialog a width
+			width: '280px'
+		});
+	}
+
+	openMoviesDialog(): void {
+		this.dialog.open(MovieCardComponent, {
+			width: '800px'
+		})
+	}
 }
