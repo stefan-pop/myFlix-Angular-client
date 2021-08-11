aa3f85ac85cf74956de98bfa8b59e364a7623b34
Stefan Pop <stefan9161@gmail.com>
2021-08-11 15:22:51 +0200

    created a method that displays the MovieCardComponent in an Angular Material dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
 	selector: 'app-root',
@@ -27,5 +28,11 @@ export class AppComponent {
 			// Assign the dialog a width
 			width: '280px'
 		});
+	}
+
+	openMoviesDialog(): void {
+		this.dialog.open(MovieCardComponent, {
+			width: '800px'
+		})
 	}
 }
