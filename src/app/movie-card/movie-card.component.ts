9a3e7675567811280c58cf14b9fef95bac85d185
Stefan Pop <stefan9161@gmail.com>
2021-08-11 15:12:48 +0200

    converted the indentation to 4 spaces

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-movie-card',
-  templateUrl: './movie-card.component.html',
-  styleUrls: ['./movie-card.component.scss']
+	selector: 'app-movie-card',
+	templateUrl: './movie-card.component.html',
+	styleUrls: ['./movie-card.component.scss']
 })
 export class MovieCardComponent implements OnInit {
 
