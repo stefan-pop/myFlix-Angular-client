31a4847a5fc35eaf4df4cbaf83347ee19c46a8b9
Stefan Pop <stefan9161@gmail.com>
2021-08-13 18:03:14 +0200

    implemented the log out functionality in the Navbar Component

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
 	selector: 'app-nav-bar',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-	constructor() { }
+	// username from local storage
+	username: string = JSON.parse(localStorage.getItem('user')!).username;
+
+	constructor(
+		public snackBar: MatSnackBar,
+		public router: Router) { }
 
 	ngOnInit(): void {
 	}
 
+	// Logout method
+	logOut(): void {
+		this.snackBar.open(`Goodbye ${this.username}`, 'OK', {
+			duration: 2000,
+			panelClass: 'success',
+			verticalPosition: 'top'
+		})
+		localStorage.clear();
+		this.router.navigate(['/welcome']);
+	}
 }
