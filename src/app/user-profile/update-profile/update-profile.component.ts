372a7d4c220b3b2e1ecf19596399bd20a76e5557
Stefan Pop <stefan9161@gmail.com>
2021-08-13 17:20:03 +0200

    imports + added a method to update user's info.
    This component is rendered in a dialog in User Profile Component

--- src/app/user-profile/update-profile/update-profile.component.ts
+++ src/app/user-profile/update-profile/update-profile.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FetchApiDataService } from 'src/app/fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
 	selector: 'app-update-profile',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateProfileComponent implements OnInit {
 
-	constructor() { }
+	// Variable that holds the new user's info from the inputs in the view.
+	@Input() newUser = {username: '', pwd:'', email:'', birth_date:''}
+
+	// Username
+	username = JSON.parse(localStorage.getItem('user')!).username;
+
+	constructor(
+		public snackbar: MatSnackBar,
+		public fetchApiData: FetchApiDataService ) { }
 
 	ngOnInit(): void {
+	}
+
+	// Method for updating user's info
+	updateUser(): void {
+		this.fetchApiData.updateUser(this.username, this.newUser).subscribe(response => {
+			localStorage.setItem('user', JSON.stringify(response));
+			this.snackbar.open('Your credentials have been updated', 'OK', {
+				duration: 1000,
+				panelClass: 'success'
+			})
+		}, (response) => {
+			console.log(response);
+			this.snackbar.open('Try again', 'OK', {
+				duration: 1000,
+				panelClass: 'error'
+			})
+		})
+		setTimeout(() => {
+			window.location.reload();
+		}, 1000)
 	}
 
 }
